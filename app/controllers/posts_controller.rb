8608b7d0a3c4da36cad1c4b72f2b25cf742e483f
teruyayuu <zhaowuyouzuo@gmail.com>
2023-11-22 06:23:58 +0900

    新規投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,12 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
   def edit
@@ -15,5 +18,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).perimt(:title, :content)
   end
 end
